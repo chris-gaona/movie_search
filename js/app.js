f1b1b058779f7f2c5aa4a0035b51154154bb59c9
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-23 20:17:11 -0700

    Adds ability to paginate search results....needs ui cleanup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(function() {
   'use strict';
 
+  $('#movies').append('<li class="no-movies">Enter a search keyword.</li>');
+
   $('#submit').on('click', function(e) {
     e.preventDefault();
 
@@ -8,7 +10,11 @@ $(function() {
     var urlSafeKeywords = searchKeywords.replace(' ', '+');
     console.log(urlSafeKeywords);
 
-    $.get("http://www.omdbapi.com/?s=" + urlSafeKeywords + "&y=&r=json", function(data) {
+    $.get("http://www.omdbapi.com/?s=" + urlSafeKeywords + "&page=2&y=&r=json", function(data) {
+      var resultAmount = data.totalResults;
+      console.log(resultAmount);
+      paginateResults(resultAmount);
+
       console.log(data);
       var searchResults = data.Search;
       console.log(searchResults);
@@ -45,5 +51,42 @@ $(function() {
     }); //$.get ()
 
   }); //.on click
+
+  function paginateResults(total) {
+    var moviesPerPage = 10;
+
+    $('#movies').empty();
+
+    //if number value passed through is less than or
+    //equal to 10 do the following...
+    if (total <= 10) {
+      //return, which stops anything after it from
+      //running...there is no need for page buttons
+      return;
+    }
+
+    var totalLinks = Math.ceil(total/moviesPerPage);
+
+    //links variable holds initial number to utilize
+    //in the while loop below...see below for more
+    var links = 0;
+    //linkNumber variable holds the intial number (1)
+    //to inject into the html using jquery
+    var linkNumber = 1;
+
+    //while links (initially 0 defined above) is less than totalLinks do the following...
+    //totalLinks in this case is by groupings of 10 students but could be
+    //some other value
+    while (links < totalLinks) {
+      //Uses jquery to append the dynamic html
+      //linkNumber was defined above & starts at 1
+      //and then every loop through gets 1 added
+      $('.main-content').append('<ul><li><a href="#">' + linkNumber++ + '</a></li></ul>');
+      //every loop until while loop is false, 1 is
+      //added to links variable until it is equal to
+      //totalLinks
+      links++;
+    }
+  }
 
 });
